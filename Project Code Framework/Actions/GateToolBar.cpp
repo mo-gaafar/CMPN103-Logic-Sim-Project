bad1e5646821fef0a7b27266972caadbdcb5d559
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2021-01-02 11:34:20 +0200

    Revert "Revert "UI improvements""

---
+++ Project Code Framework/Actions/GateToolBar.cpp
@@ -0,0 +1,49 @@
+#include "GateToolBar.h"
+
+/* Constructor */
+GateToolBar::GateToolBar(ApplicationManager* pAppMan) : Action(pAppMan) {
+
+}
+
+/* Reads parameters required for action to execute */
+void GateToolBar::ReadActionParameters()
+{
+	//Get a Pointer to the Input / Output Interfaces
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
+
+	//Prints action message
+	pOut->PrintMsg("View add gate toolbar");
+
+	//Wait for User Input
+	pIn->GetPointClicked(Cx, Cy);
+
+	//Clear Status Bar
+	pOut->ClearStatusBar();
+
+}
+
+/* Executes action */
+void GateToolBar::Execute() {
+	ReadActionParameters();
+
+	Output* pOut = pManager->GetOutput();
+	pOut->ClearToolBar();
+	pOut->CreateGateToolBar();
+
+}
+
+/* Undo action */
+void GateToolBar::Undo() {
+	return;
+}
+
+/* Redo action */
+void GateToolBar::Redo() {
+	return;
+}
+
+/* Destructor */
+GateToolBar::~GateToolBar() {
+
+}
